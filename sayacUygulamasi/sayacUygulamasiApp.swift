fcd02c902ff362f2d24f1699d3d2dbc6a2edd246
BilalZeydKlc <bilalzeydd@yaani.com>
2026-02-16 11:00:23 +0300

    Initial Commit

---
+++ sayacUygulamasi/sayacUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  sayacUygulamasiApp.swift
+//  sayacUygulamasi
+//
+//  Created by Bilal Zeyd Kılıç on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct sayacUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
